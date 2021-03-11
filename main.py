f303050ee2d064aee9f082c8048be381ddb13487
Jason Liu <jasonl9k@uw.edu>
2021-03-10 17:52:22 -0800

    finished other methods

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 
 # C:\\Users\\Jason Liu\\PycharmProjects\\cse163-final-project\\processed.cleveland.csv
 
-#Hello. It's Jason.
+# Hello. It's Jason.
 
 import pandas as pd
 from sklearn.model_selection import cross_val_score
@@ -20,56 +20,64 @@ import matplotlib
 import scipy
 import numpy as np
 
+
 def main():
+    # Reads in the file
     data = pd.read_csv('C:\\Users\\Jason Liu\\PycharmProjects\\cse163-final-project\\processed.cleveland.csv')
 
+    # Drops '?' datapoints
     data = data.replace('?', np.NaN)
     data = data.dropna()
 
-    all_features = data.copy()
-    all_features = all_features.loc[:, data.columns != 'num']
-    all_features = pd.get_dummies(all_features, columns=['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal'])
-    #print(all_features)
+    # Features
+    features = data.copy()
+    features = features.loc[:, data.columns != 'num']
+    features_list = features.columns
+    categorical_variables = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal']
+    features = pd.get_dummies(features, columns=categorical_variables)
 
+    # Labels
     labels = data['num']
 
-    clf = RandomForestClassifier(n_estimators=5)
+    # Stores all score values
+    all_results = {}
 
-    features_train, features_test, labels_train, labels_test = train_test_split(all_features, labels, test_size=0.3)
-
-    clf = clf.fit(features_train, labels_train)
-
-    test_score = clf.score(features_test, labels_test)
-    #print(test_score)
-
-    scores = cross_val_score(clf, features_test, labels_test, cv=2)
-
-
-    model = DecisionTreeClassifier()
-    model.fit(features_train, labels_train)
-    label_predictions = model.predict(features_test)
-    train_acc = accuracy_score(labels_test, label_predictions)
-    print('Accuracy:', train_acc)
-
-    #print(scores.mean())
+    # Model with all features
+    all_results['all'] = []
+    for i in range(10):
+        clf = RandomForestClassifier(n_estimators=10, random_state=1)
+        features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3)
+        clf = clf.fit(features_train, labels_train)
+        test_score = clf.score(features_test, labels_test)
+        all_results['all'].append(test_score)
 
 
+def create_model(dropped_column, data, results, iterations):
+    features, labels = process_data(data, dropped_column)
+    if dropped_column is None:
+        dropped_column = 'all'
+    if dropped_column not in results:
+        results[dropped_column] = []
+    for i in range(iterations):
+        clf = RandomForestClassifier(n_estimators=10)
+        features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3)
+        clf = clf.fit(features_train, labels_train)
+        test_score = clf.score(features_test, labels_test)
+        results[dropped_column].append(test_score)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def process_data(data, dropped_column):
+    features = data.copy()
+    features = features.loc[:, data.columns != 'num']
+    features_list = features.columns
+    categorical_variables = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal']
+    if dropped_column is not None:
+        features_list.remove(dropped_column)
+        if dropped_column in categorical_variables:
+            categorical_variables.remove(dropped_column)
+    features = pd.get_dummies(features, columns=categorical_variables)
+    labels = data['num']
+    return features, labels
 
 
 if __name__ == '__main__':
