1296fd058364201e30b0778533c366cf9b60b930
Jason Liu <jasonl9k@uw.edu>
2021-03-16 00:59:29 -0700

    3/15 part 2

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ import numpy as np
 
 def main():
     # Reads in the file
-    data = pd.read_csv('https://raw.githubusercontent.com/yoonseosong/cse163-final-project/master/processed.cleveland'
-                       '.csv')
+    data = pd.read_csv('https://raw.githubusercontent.com/yoonseosong/cse163-'
+                       'final-project/master/processed.cleveland.csv')
 
     # Drops '?' data points
     data = data.replace('?', np.NaN)
@@ -49,29 +49,13 @@ def main():
     # Creates a boxplot of the results and finds the p-value through ANOVA
     analyze_results(all_results)
 
-    print(all_results)
-
-    row_name = ['mean', 'sd']
-    cell_text = [[], []]
-    for column in all_results.columns:
-        curr_column = all_results[[column]].transpose()
-        curr_column = curr_column.values.tolist()[0]
-        cell_text[0].append(round(sum(curr_column) / len(curr_column), 4))
-        cell_text[1].append(round(np.std(curr_column), 4))
-
-    fig, ax1 =plt.subplots(1, 1)
-    ax1.axis('tight')
-    ax1.axis('off')
-    ax1.table(cellText=cell_text, colLabels=all_results.columns, rowLabels=row_name, loc="center")
-
-    plt.show()
-
-
 
 def create_model(dropped_column, data, results, iterations):
     '''
-    Creates a ML model using the inputted data while dropping the specified column
-    Creates the specified number (iterations) of models and stores the results in results
+    Creates a ML model using the inputted data while dropping
+    the specified column
+    Creates the specified number (iterations) of models and stores
+    the results in results
     '''
     features, labels = process_data(data, dropped_column)
     if dropped_column is None:
@@ -79,12 +63,16 @@ def create_model(dropped_column, data, results, iterations):
     if dropped_column not in results:
         results[dropped_column] = []
     for i in range(iterations):
+        '''
+        Uncomment 1 classifier at a time
+        '''
         clf = RandomForestClassifier(n_estimators=10)
         # clf = KNeighborsClassifier()
         # clf = MLPClassifier()
         # clf = AdaBoostClassifier()
         # clf = DecisionTreeClassifier()
-        features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3)
+        features_train, features_test, labels_train, labels_test =\
+            train_test_split(features, labels, test_size=0.3)
         clf = clf.fit(features_train, labels_train)
         test_score = clf.score(features_test, labels_test)
         results[dropped_column].append(test_score)
@@ -100,7 +88,8 @@ def process_data(data, dropped_column):
     features = data.copy()
     features = features.loc[:, data.columns != 'num']
     features_list = list(features.columns)
-    categorical_variables = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal']
+    categorical_variables = ['sex', 'cp', 'fbs', 'restecg', 'exang',
+                             'slope', 'thal']
     if dropped_column is not None:
         features_list.remove(dropped_column)
         if dropped_column in categorical_variables:
@@ -114,14 +103,21 @@ def analyze_results(all_results):
     '''
     Takes in the results from our ML models and runs an ANOVA test
     Plots the results as a boxplot along with the ANOVA p value
+    Creates a table with means and standard deviations
     '''
 
     # ANOVA
-    f_value, p_value = stats.f_oneway(all_results['none'], all_results['age'], all_results['sex'], all_results['cp'],
-                                    all_results['trestbps'], all_results['chol'], all_results['fbs'],
-                                    all_results['restecg'], all_results['thalach'], all_results['exang'],
-                                    all_results['oldpeak'], all_results['slope'], all_results['ca'],
-                                    all_results['thal'])
+    f_value, p_value = stats.f_oneway(all_results['none'], all_results['age'],
+                                      all_results['sex'], all_results['cp'],
+                                      all_results['trestbps'],
+                                      all_results['chol'], all_results['fbs'],
+                                      all_results['restecg'],
+                                      all_results['thalach'],
+                                      all_results['exang'],
+                                      all_results['oldpeak'],
+                                      all_results['slope'],
+                                      all_results['ca'],
+                                      all_results['thal'])
 
     # Plot a boxplot of model accuracies
     results_melt = pd.melt(all_results.reset_index(), id_vars=['index'], value_vars=list(all_results.columns))
@@ -132,7 +128,24 @@ def analyze_results(all_results):
     plt.ylabel('Accuracy Score')
     plt.xticks(rotation=-30)
     plt.tight_layout()
-    # plt.show()
+
+    # Creates a table for the accuracy scores with the mean and standard deviations
+    row_name = ['mean', 'sd']
+    cell_text = [[], []]
+    for column in all_results.columns:
+        curr_column = all_results[[column]].transpose()
+        curr_column = curr_column.values.tolist()[0]
+        cell_text[0].append(round(sum(curr_column) / len(curr_column), 3))
+        cell_text[1].append(round(np.std(curr_column), 3))
+    fig, ax1 = plt.subplots(1, 1, figsize=(15, 8))
+    ax1.xaxis.set_label_position('top')
+    ax1.set_title('Means and Standard Deviations of Accuracy Scores')
+    # plt.title('Means and Standard Deviations of Accuracy Scores')
+    ax1.axis('tight')
+    ax1.axis('off')
+    ax1.table(cellText=cell_text, colLabels=all_results.columns, rowLabels=row_name, loc="center")
+
+    plt.show()
 
 
 if __name__ == '__main__':
